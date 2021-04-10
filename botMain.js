671d22433a139c82588368e24e481bb9c4dfe55b
Robert Fink <rwfink4@umd.edu>
2021-04-10 14:55:53 -0400

    added galaxy brain functionality

--- botMain.js
+++ botMain.js
@@ -3,7 +3,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Beep Bop Bot is ready')
+    console.log('Beep Bop Bot is ready.')
+});
+
+client.on('message', msg => {
+    if(msg.content == 'galaxy brain') {
+	const galaxyBrain = new Discord.MessageAttachment('galaxyBrain.jpg');
+	msg.channel.send(galaxyBrain);
+    }
 });
 
 client.login(process.env.BOTTOKEN);
