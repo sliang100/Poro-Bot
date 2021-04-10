37bb7ab07db4b77749f780cd705072eaa6befd33
Robert Fink <rwfink4@umd.edu>
2021-04-10 14:02:18 -0400

    bot now starts

--- botMain.js
+++ botMain.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Bot is ready');
+    console.log('Beep Bop Bot is ready')
 });
 
-client.login(process.env.botToken);
+client.login(process.env.BOTTOKEN);
