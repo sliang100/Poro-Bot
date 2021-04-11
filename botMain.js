0e45eceff4e64f6e4c2543284d24914f0a4f21c5
Robert Fink <rwfink4@umd.edu>
2021-04-11 01:37:52 -0400

    trying to get a alarm to work

--- botMain.js
+++ botMain.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
+
 const Discord = require('discord.js');
+const Agenda = require('agenda');
 const client = new Discord.Client();
+const mongoConnectionString = 'mongodb://127.0.0.1:27017/AgendaMedium';
+const agenda = new Agenda({
+    db: {address: mongoConnectionString, collection: 'Agenda'},
+    processEvery: '20 seconds',
+    useUnifiedTopology: true
+});
 
 client.on('ready', () => {
-    console.log('Beep Bop Bot is ready.')
+    console.log('Beep Bop Bot is ready.');
 });
 
 client.on('message', msg => {
@@ -16,6 +24,64 @@ client.on('message', msg => {
     } else if (msg.content == 'stonks') {
        const stonksImg = new Discord.MessageAttachment('media/gooseStonks.jpg');
        msg.channel.send(stonksImg);
+    } else if (msg.content.startsWith('!setAlarm')){
+	let commandStr = msg.content;
+	//command format: !setAlarm [hour]:[minute] [month]/[day]/[year]
+	let timeStr = commandStr.substring(10);
+
+	let i = 0;
+	let month = '', day = '', year = '', hour = '', minute = '';
+
+	while(timeStr.charAt(i) != ':' && i < timeStr.length) {
+	    hour = hour + timeStr.charAt(i);
+	    i++;
+	}
+	i++;
+
+	while(timeStr.charAt(i) != ' ' && i < timeStr.length) {
+	    minute = minute + timeStr.charAt(i);
+	    i++;
+	}
+	i++;
+
+	while(timeStr.charAt(i) != '/' && i < timeStr.length) {
+	    month = month + timeStr.charAt(i);
+	    i++;
+	}
+	i++;
+
+	while(timeStr.charAt(i) != '/' && i < timeStr.length) {
+	    day = day + timeStr.charAt(i);
+	    i++;
+	}
+	i++
+
+	while(i < timeStr.length) {
+	    year = year + timeStr.charAt(i);
+	    i++;
+	}
+
+	if(hour == '' || minute == '') {
+	    console.error('insufficent time data');
+	} else if(month == '' || day == '') {
+	    //set day, month, and year to current day, month, and year
+	    let now = new Date();
+	    let monthInt = now.getMonth() + 1;
+	    let dayInt = now.getDate();
+	    let yearInt = now.getFullYear();
+	    
+	    month = monthInt.toString();
+	    day = dayInt.toString();
+	    year = yearInt.toString();
+	} else if(year == '' ) {
+	    //set year to current year
+	    let now = new Date();
+	    let yearInt = now.getFullYear();
+
+	    year = yearInt.toString();
+	} else {
+	    //run with all user data
+	}
     } else {
 	//do nothing
     }
