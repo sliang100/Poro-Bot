f66e92fe0706118199e9d80d490b4b4683da342f
Robert Fink <rwfink4@umd.edu>
2021-04-11 05:39:49 -0400

    got basic alarm workinglsls

--- test.js
+++
@@ -1,9 +0,0 @@
-const Agenda = require('agenda');
-
-const dbURL = 'mongodb://127.0.0.1:27017/AgendaMedium';
-
-const agenda = new Agenda({
-    db: {address: dbURL, collection: 'Agenda'},
-    processEvery: '20 seconds',
-    useUnifiedTopology: true
-});
